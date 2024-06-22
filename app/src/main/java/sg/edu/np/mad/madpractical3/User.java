864708d2141639456ca0ea0791637aab2b912786
cshida <s10241591@connect.np.edu.sg>
2024-06-22 16:52:43 +0800

    done

---
+++ app/src/main/java/sg/edu/np/mad/madpractical3/User.java
@@ -0,0 +1,17 @@
+package sg.edu.np.mad.madpractical3;
+
+public class User {
+    public String name;
+    public String id;
+    public int description;
+    public boolean followed;
+    public User(String name,String id, int description, boolean followed){
+        this.name = name;
+        this.id = id;
+        this.description = description;
+        this.followed= followed;
+
+
+    }
+
+}
